6dc855c91ab9087c936472241caf2931d7931dba
iOSDeveloper <dovuthanh2018@gmail.com>
2024-02-23 00:29:29 +0700

    update timeout

--- index_whole_flow.js
+++ index_whole_flow.js
@@ -53,7 +53,7 @@ const main = async () => {
 				await page.goto(url, {waitUntil: 'networkidle0'});
 				const element = await page.waitForSelector('#continue-button-id');
 				await element.click();
-				const searchResult = await page.waitForSelector('#total-count-id');
+				const searchResult = await page.waitForSelector('#total-count-id', {timeout: 10000000});
 				const htmlContent = await page.content();
 				const collection = {
 					url: url,
